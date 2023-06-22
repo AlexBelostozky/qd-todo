2944bff29f77a44af0e5f40ce2d93b19f69f13d1
Александр Белостоцкий <belostozky1994@mail.ru>
2023-06-22 19:31:18 +0600

    Fix 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -13,7 +13,10 @@ const routes = [
   {
     path: '/tasks',
     name: 'tasks',
-    component: () => import('../views/qdTasks.vue')
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/qdTasks.vue')
   }
 ]
 
